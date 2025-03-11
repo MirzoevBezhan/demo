13a1e2aa8e69bdfb39bd94106f011f630af1525d
Bezhan_Mirzoev <assas1ndarknesss@gmail.com>
2025-03-11 16:22:17 +0500

    first commit

--- Infastructure/obj/Debug/net9.0/Infastructure.AssemblyInfo.cs
+++ Infastructure/obj/Debug/net9.0/Infastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c6601e5428aaa3bf31235d08ffee02eef70b148")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
